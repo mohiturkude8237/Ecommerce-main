e9bcd60c94d494c26c494e2435ed5060be5a3902
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-06-29 20:15:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-center bg-yellow-500 text-red-600 px-6 py-7'>
+      Hello I am App
+    </div>
+  )
+}
+
+export default App
